d19aae6ebd6126c94f4059d0fca7014ecc196b96
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-04-30 17:27:50 -0300

    authjson fixed

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const authConfig = require("../../config/auth.json");
+const { AUTHTOKEN } = process.env;
 
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -17,7 +17,7 @@ module.exports = (req, res, next) => {
     return res.status(401).send({ error: "Token malformatted" });
   }
 
-  jwt.verify(token, authConfig.secret, (err, decoded) => {
+  jwt.verify(token, AUTHTOKEN, (err, decoded) => {
     if (err) return res.status(401).send({ error: "Token invalid" });
 
     req.userId = decoded.id;
