b34be2249baf9c726141197b2ef22302c21877f5
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-05-21 11:33:11 -0300

    fixed token route

--- src/public/index.js
+++ src/public/index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { urlencoded } = require("body-parser");
 
+
 const app = express();
 
 app.use(express.json());
 app.use(urlencoded({ extended: false }));
 
-require("./app/controllers/index")(app);
+require("../app/http/controllers/index")(app);
 
 app.listen(3000);
